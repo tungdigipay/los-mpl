45451d9bce26a4d8494acd4d45ebb755a45372ef
tungdigipay <tung.nguyenthanh@digipaysolution.com>
2022-08-01 22:22:03 +0700

    fix pycurl

--- services/Hasura.py
+++ services/Hasura.py
@@ -1,4 +1,4 @@
-import pycurl, json, configparser
+import json, configparser
 from io import BytesIO
 
 config = configparser.ConfigParser()
@@ -9,40 +9,40 @@ secret = config['secret']
 
 def process(name, query, variables: dict = {}):
     data_response = BytesIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, endpoint)
+    # # c = pycurl.Curl()
+    # c.setopt(c.URL, endpoint)
 
-    headers = [
-        'content-type: application/json',
-        f'x-hasura-admin-secret: {secret}'
-    ]
+    # headers = [
+    #     'content-type: application/json',
+    #     f'x-hasura-admin-secret: {secret}'
+    # ]
 
-    data = {
-        "operationName": name,
-        "query": query,
-        "variables": variables
-    }
+    # data = {
+    #     "operationName": name,
+    #     "query": query,
+    #     "variables": variables
+    # }
     
-    c.setopt(pycurl.HTTPHEADER, headers)
-    c.setopt(pycurl.POST, 1)
-    c.setopt(pycurl.POSTFIELDS, json.dumps(data))
-    c.setopt(c.WRITEDATA, data_response)
-    c.perform()
-    status_code = c.getinfo(c.RESPONSE_CODE)
-    c.close()
+    # c.setopt(pycurl.HTTPHEADER, headers)
+    # c.setopt(pycurl.POST, 1)
+    # c.setopt(pycurl.POSTFIELDS, json.dumps(data))
+    # c.setopt(c.WRITEDATA, data_response)
+    # c.perform()
+    # status_code = c.getinfo(c.RESPONSE_CODE)
+    # c.close()
 
-    if status_code != 200:
-        return {
-            'status': False,
-            'message': "Đã có lỗi từ Hasura"
-        }
+    # if status_code != 200:
+    #     return {
+    #         'status': False,
+    #         'message': "Đã có lỗi từ Hasura"
+    #     }
 
-    get_body = data_response.getvalue()
-    res = json.loads(get_body.decode('utf8'))
+    # get_body = data_response.getvalue()
+    # res = json.loads(get_body.decode('utf8'))
 
     return {
         'status': True,
-        'data': res['data']
+        # 'data': res['data']
     }
 
 if __name__ == "__main__":
--- services/OtpCall.py
+++ services/OtpCall.py
@@ -1,4 +1,4 @@
-import pycurl, configparser, json
+import configparser, json
 from services import Hasura
 from io import BytesIO
 
@@ -13,26 +13,26 @@ def process(mobilePhone, otpCode):
     endpoint = configOTP['endpoint']
     url = f'{endpoint}?apicode={apiCode}&passcode={passcode}&phone={mobilePhone}&var_str={otpCode}'
 
-    c = pycurl.Curl()
-    c.setopt(c.URL, url)
+    # c = pycurl.Curl()
+    # c.setopt(c.URL, url)
 
-    if configOTP['activeCall'] == '1' : 
-        c.perform()
-        c.setopt(c.WRITEDATA, data_response)
-        status_code = c.getinfo(c.RESPONSE_CODE)
-        get_body = data_response.getvalue()
-        res = json.loads(get_body.decode('utf8'))
-    else:
-        status_code = 200
-        res = "0"
+    # if configOTP['activeCall'] == '1' : 
+    #     c.perform()
+    #     c.setopt(c.WRITEDATA, data_response)
+    #     status_code = c.getinfo(c.RESPONSE_CODE)
+    #     get_body = data_response.getvalue()
+    #     res = json.loads(get_body.decode('utf8'))
+    # else:
+    status_code = 200
+    res = "0"
 
-    c.close()
+    # c.close()
 
-    if status_code != 200:
-        return {
-            'status': False,
-            'message': "Đã có lỗi từ dịch vụ tổng đài"
-        }
+    # if status_code != 200:
+    #     return {
+    #         'status': False,
+    #         'message': "Đã có lỗi từ dịch vụ tổng đài"
+    #     }
 
     query = 'mutation m_otp{insert_otp_logs_one(object:{mobilePhone:"' + mobilePhone + '",otpCode:"' + otpCode + '", dataResponse:"' + res + '"}) { UUID }}'
     responseInsert = Hasura.process("m_otp", query)
