4c19498684ba725ea7c811e2e92b3f80d9b11aaa
tungdigipay <tung.nguyenthanh@digipaysolution.com>
2022-08-01 23:06:11 +0700

    update response

--- services/OtpCall.py
+++ services/OtpCall.py
@@ -28,7 +28,6 @@ def process(mobilePhone, otpCode):
 
     query = 'mutation m_otp{insert_otp_logs_one(object:{mobilePhone:"' + mobilePhone + '",otpCode:"' + otpCode + '", dataResponse:"' + res + '"}) { UUID }}'
     responseInsert = Hasura.process("m_otp", query)
-    return responseInsert
     
     return {
         'status': True,
