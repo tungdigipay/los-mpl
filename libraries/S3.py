40ed016614f094c3af449058f7f8c9374449e615
tungdigipay <tung.nguyenthanh@digipaysolution.com>
2022-08-24 13:50:59 +0700

    commit create bucket

--- libraries/S3.py
+++ libraries/S3.py
@@ -7,17 +7,20 @@ config.read('configs.ini')
 configS3 = config['S3']
 bucket = configS3['aws_bucket']
 
-def upload(file, name):
-    s3_client = boto3.client('s3', 
-        endpoint_url=configS3['aws_endpoint'],
-        aws_access_key_id = configS3['aws_access_key_id'],
-        aws_secret_access_key = configS3['aws_secret_access_key'],
-        region_name = configS3['aws_region']
-    )
+s3_client = boto3.client('s3', 
+    endpoint_url=configS3['aws_endpoint'],
+    aws_access_key_id = configS3['aws_access_key_id'],
+    aws_secret_access_key = configS3['aws_secret_access_key'],
+    region_name = configS3['aws_region']
+)
 
+def upload(file, name):
     try:
         response = s3_client.upload_file(file, bucket, name, ExtraArgs={'ACL':'public-read'})
     except ClientError as e:
         logging.error(e)
 
     return f"{configS3['aws_endpoint']}/{bucket}/{name}"
+
+def create_bucket():
+    return s3_client.create_bucket(Bucket="bnpl.profiles")
