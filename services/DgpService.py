2843cf5a004e2b36b9aba4b345b6e32ccbd7cfbd
tungdigipay <tung.nguyenthanh@digipaysolution.com>
2022-08-28 17:30:11 +0700

    Update DgpService.py

--- services/DgpService.py
+++ services/DgpService.py
@@ -1,30 +1,11 @@
-from services import ApplicationService, ScoringLogService
+from services import ApplicationService
 
 def check_blacklist(applicationID, idNumber, mobilePhone):
-    res = ApplicationService.mfast_blacklist(idNumber, mobilePhone)
-
-    payload = {
-        "idNumber": idNumber, 
-        "mobilePhone": mobilePhone
-    }
-
-    result_content = {}
-    result_content["message"] = res['message']
-    if res['status'] == True: 
-        result = "pass" 
-    else: 
-        result = "rejected"
-    
-    ScoringLogService.record({
-        "applicationID": applicationID, 
-        "result": result, 
-        "type": "dgp_blacklist", 
-        "label": "DGP Blacklist", 
-        "logServiceID": 0
-    }, payload, result_content)
-    
-    return res
+    return ApplicationService.mfast_blacklist(applicationID, idNumber, mobilePhone)
 
 def check_dedup(idNumber, mobilePhone):
     res = ApplicationService.mfast_dedup(idNumber, mobilePhone)
     return res
+
+def gotit():
+    pass
